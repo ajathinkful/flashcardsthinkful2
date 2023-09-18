1644a52caa41f5ff226f250fee2cf91b36e8f276
Alex <aja44@pitt.edu>
2023-09-18 17:37:47 -0400

    study commit

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import NotFound from "./NotFound";
 import Home from "../Home"; // Import the Home component
 import CreateDeck from "./CreateDeck"; // Import the CreateDeck component
+import Study from "../Study"; // Import the Study component
 
 function Layout() {
   return (
@@ -12,17 +13,18 @@ function Layout() {
       <div className="container">
         <Switch>
           <Route path="/" exact>
-            {" "}
             {/* This is the Home route */}
             <Home />
           </Route>
           <Route path="/decks/new">
-            {" "}
             {/* This is the Create Deck route */}
             <CreateDeck />
           </Route>
+          <Route path="/decks/:deckId/study">
+            {/* This is the Study route */}
+            <Study />
+          </Route>
           <Route path="*">
-            {" "}
             {/* This is a catch-all route for 404 Not Found */}
             <NotFound />
           </Route>
--- src/Study.js
+++ src/Study.js
@@ -1,57 +1,56 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { readDeck } from "./utils/api";
+import { readDeck } from "./utils/api/index";
 
 function Study() {
   const { deckId } = useParams();
   const [deck, setDeck] = useState(null);
-  const [currentCardIndex, setCurrentCardIndex] = useState(0);
-  const [isFlipped, setIsFlipped] = useState(false);
+  const [cardIndex, setCardIndex] = useState(0);
 
   useEffect(() => {
-    const fetchData = async () => {
+    async function fetchData() {
       try {
         const loadedDeck = await readDeck(deckId);
         setDeck(loadedDeck);
       } catch (error) {
         console.error("Error fetching deck:", error);
       }
-    };
-
+    }
     fetchData();
   }, [deckId]);
 
-  const currentCard = deck?.cards[currentCardIndex];
-
-  const handleFlip = () => {
-    setIsFlipped(!isFlipped);
-  };
-
   const handleNext = () => {
-    if (currentCardIndex < deck.cards.length - 1) {
-      setCurrentCardIndex(currentCardIndex + 1);
-      setIsFlipped(false);
+    if (cardIndex < deck.cards.length - 1) {
+      setCardIndex(cardIndex + 1);
     } else {
-      // Reached end of cards
-      alert("End of cards");
+      const restart = window.confirm("Restart cards?");
+
+      if (restart) {
+        setCardIndex(0);
+      }
     }
   };
 
+  if (!deck) {
+    return <div>Loading...</div>;
+  }
+
+  const card = deck.cards[cardIndex];
+
   return (
     <div>
-      <h2>Study Screen</h2>
-      {deck && (
-        <>
-          <h3>{deck.name}</h3>
-          <div>
-            <div>
-              <p>{isFlipped ? currentCard.back : currentCard.front}</p>
-            </div>
-            <button onClick={handleFlip}>Flip</button>
-          </div>
-          <button onClick={handleNext}>Next</button>
-        </>
-      )}
+      <h1>Study: {deck.name}</h1>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">
+            Card {cardIndex + 1} of {deck.cards.length}
+          </h5>
+          <p className="card-text">{card.front}</p>
+          <button className="btn btn-secondary" onClick={handleNext}>
+            Next
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
